fd93be4c8d7c16e02b95e0666308c1a3d11138ce
DmitriySavin <debilv556@gmail.com>
2026-02-16 16:06:44 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
-import { WeatherHaracteristics } from './components/WeatherHaracteristics/WeatherHaracteristics'
+import "./App.css";
+import { WeatherHaracteristics } from "./components/WeatherHaracteristics/WeatherHaracteristics";
+
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import Header from "./components/Header/Header";
+import News from "./components/News/News";
 
 function App() {
-
   return (
-    <div className='container'>
-   <WeatherHaracteristics />
-    </div>
-  )
+    <>
+      {/* <div className="container"> */}
+        {/* <WeatherHaracteristics /> */}
+     
+      
+      <Header />
+      <News />
+        <ToastContainer position="top-right" autoClose={2000} />
+         {/* </div> */}
+    </>
+  );
 }
 
-export default App
+export default App;
