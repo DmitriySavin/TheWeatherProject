74da9beba0e03584781d160bb217426b802c003b
DesertKnight <146014154+Poseydon12@users.noreply.github.com>
2026-02-21 10:02:19 +0100

    done section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import WeatherHero from './components/WeatherHero/WeatherHero'
 
 function App() {
 
   return (
     <>
-   
+   <WeatherHero />
     </>
   )
 }
---
+++ src/services/citysearch.js
@@ -0,0 +1,19 @@
+const BASE_API = "9abb2e573db1c5f1f867196375388bbe";
+const BASE_URL = "https://api.openweathermap.org";
+
+const fetcharticleApi = (name) => {
+  return fetch(
+    `${BASE_URL}/data/2.5/weather?q=${name}&appid=${BASE_API}&units=metric`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("City not found");
+      }
+      return res.json();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export default fetcharticleApi;
