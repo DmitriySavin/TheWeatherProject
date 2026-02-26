48f969bc47b875948e85b116b6354f58f35bd53c
Denis Karatyha <karatygad@gmail.com>
2026-02-26 15:53:50 +0100

    News

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,40 @@
-import "./App.css";
-import { ToastContainer } from "react-toastify";
-import Header from "./components/Header/Header";
-
-function App() {
-  return (
-    <>
-      <Header />
-      <ToastContainer position="top-center" autoClose={1000} />
-    </>
-  );
-}
-
-export default App;
-
-
-// import "./App.css";
-
-// import { WeatherHaracteristics } from "./components/WeatherHaracteristics/WeatherHaracteristics";
 // import "./App.css";
 // import { ToastContainer } from "react-toastify";
 // import Header from "./components/Header/Header";
-// import News from "./components/News/News";
 
 // function App() {
 //   return (
 //     <>
-
-//       {/* <div className="container"> */}
-//         {/* <WeatherHaracteristics /> */}
-
 //       <Header />
-//       <News />
-//         <ToastContainer position="top-right" autoClose={2000} />
-//          {/* </div> */}
-
-//       <Header />
-//       <News />
-//       <ToastContainer position="top-right" autoClose={2000} />
-
+//       <ToastContainer position="top-center" autoClose={1000} />
 //     </>
 //   );
 // }
 
 // export default App;
+
+import "./App.css";
+
+import { WeatherHaracteristics } from "./components/WeatherHaracteristics/WeatherHaracteristics";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import Header from "./components/Header/Header";
+import News from "./components/News/News";
+import WeatherHero from "./components/WeatherHero/WeatherHero";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <WeatherHaracteristics />
+        <WeatherHero />
+
+        <Header />
+        <News />
+        <ToastContainer position="top-right" autoClose={2000} />
+      </div>
+    </>
+  );
+}
+
+export default App;
