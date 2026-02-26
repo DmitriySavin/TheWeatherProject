5e22909566359750b685421277ac850101b7c804
DesertKnight <146014154+Poseydon12@users.noreply.github.com>
2026-02-26 20:01:02 +0100

    done fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Header from './components/Header/Header'
+import Modal from './components/Modal/Modal'
+import WeatherHero from './components/WeatherHero/WeatherHero'
 
 function App() {
 
   return (
     <>
-   
+   <WeatherHero />
+   <Header />
     </>
   )
 }
