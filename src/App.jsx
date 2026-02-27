0c561fb16a499ebe836e388cca4f96435b040ac4
DmitriySavin <debilv556@gmail.com>
2026-02-27 11:33:20 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,4 @@
 import "./App.css";
-import { ToastContainer } from "react-toastify";
-import Header from "./components/Header/Header";
-
-function App() {
-  return (
-    <>
-      <Header />
-      <ToastContainer position="top-center" autoClose={1000} />
-    </>
-  );
-}
-
-export default App;
-
-
-// import "./App.css";
-
-// import { WeatherHaracteristics } from "./components/WeatherHaracteristics/WeatherHaracteristics";
-// import "./App.css";
-// import { ToastContainer } from "react-toastify";
-// import Header from "./components/Header/Header";
-
-// function App() {
-//   return (
-//     <>
-//       <Header />
-//       <ToastContainer position="top-center" autoClose={1000} />
-//     </>
-//   );
-// }
-
-// export default App;
-
-import "./App.css";
 
 import { WeatherHaracteristics } from "./components/WeatherHaracteristics/WeatherHaracteristics";
 import "./App.css";
